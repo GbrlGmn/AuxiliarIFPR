bdd22db978ac534c7bc4c9df5c8207141d3cee30
gbrlgmn <gabegol307@gmail.com>
2026-06-03 11:48:16 -0300

    Colocando combobox em estado

--- cadastrarCliente.php
+++ cadastrarCliente.php
@@ -76,14 +76,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <label for="cidade" class="form-label">Cidade:</label>
                     <input type="text" class="form-control" id="cidade" name="cidade" required>
                 </div>
-                <div class="mb-3">
-                    <label for="endereco" class="form-label">Endereço:</label>
-                    <input type="text" class="form-control" id="endereco" name="endereco" required>
-                </div>
-                <div class="mb-3">
-                    <label for="estado" class="form-label">Estado:</label>
-                    <input type="text" class="form-control" id="estado" name="estado" required>
-                </div>
+                <label for="estado" class="form-label">Estado:</label>
+                <select class="form-select mb-3" aria-label="Default select example" name="estado" required>
+                    
+                <option selected>-</option>
+  <option value="1">Acre (AC)</option>
+  <option value="2">Alagoas (AL)</option>
+  <option value="3">Amapá (AP)</option>
+  <option value="4">Amazonas (AM)</option>
+  <option value="5">Bahia (BA)</option>
+  <option value="6">Ceará (CE)</option>
+  <option value="7">Distrito Federal (DF)</option>
+  <option value="8">Espírito Santo (ES)</option>
+  <option value="9">Goiás (GO)</option>
+  <option value="10">Maranhão (MA)</option>
+  <option value="11">Mato Grosso (MT)</option>
+  <option value="12">Mato Grosso do Sul (MS)</option>
+  <option value="13">Minas Gerais (MG)</option>
+  <option value="14">Pará (PA)</option>
+  <option value="15">Paraíba (PB)</option>
+  <option value="16">Paraná (PR)</option>
+  <option value="17">Pernambuco (PE)</option>
+  <option value="18">Piauí (PI)</option>
+  <option value="19">Rio de Janeiro (RJ)</option>
+  <option value="20">Rio Grande do Norte (RN)</option>
+  <option value="21">Rio Grande do Sul (RS)</option>
+  <option value="22">Rondônia (RO)</option>
+  <option value="23">Roraima (RR)</option>
+  <option value="24">Santa Catarina (SC)</option>
+  <option value="25">São Paulo (SP)</option>
+  <option value="26">Sergipe (SE)</option>
+  <option value="27">Tocantins (TO)</option>
+            </select>
                 <div class="mb-3">
                     <label for="observacao" class="form-label">Observação:</label>
                     <textarea class="form-control" id="observacao" name="observacao" rows="3" required></textarea>
